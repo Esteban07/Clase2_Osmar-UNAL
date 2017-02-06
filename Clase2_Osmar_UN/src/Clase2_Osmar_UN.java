ace41358046fc6d2520f4d07ffb6629b268d400e
Estudiante <Estudiante@10.203.1.236>
2017-02-06 14:35:33 -0500

    Prueba inicial

--- Clase2_Osmar_UN/src/Clase2_Osmar_UN.java
+++ Clase2_Osmar_UN/src/Clase2_Osmar_UN.java
@@ -1,6 +1,6 @@
  public class Clase2_Osmar_UN {
           public static void main(String[] args) { 
-            System.out.println("Hola Mundo");
+            System.out.println("Hola Mundo :)");
        }
 } 
 
